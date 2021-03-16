0460a8ffd5c15f1048dd1d519a1c21261cf5583a
Masahiro Honda <honda@mechatrax.com>
2021-03-16 14:11:59 +0900

    Add AT command functions to 4gpictl

--- device-stop.sh
+++ device-stop.sh
@@ -1,53 +1,26 @@
 #!/bin/bash
 
-TIMEOUT=20
-
-ABORT="ABORT BUSY ABORT ERROR"
-SCRIPT_FNCMIN="'' AT OK-AT-OK AT+CFUN=0 OK ''"
-SCRIPT_PWROFF="'' AT OK-AT-OK AT+CPOF OK ''"
-SCRIPT_RI="'' AT OK-AT-OK AT+CFGRI=1 OK AT+CSCLK=1 OK ''"
-SCRIPT_SM="'' AT OK-AT-OK AT+CNMI=1,2,0,0,0 OK AT+CPMS=\"SM\",\"SM\",\"SM\" OK ''"
-SCRIPT_ME="'' AT OK-AT-OK AT+CNMI=2,1,2,2,0 OK AT+CPMS=\"ME\",\"ME\",\"ME\" OK ''"
-
-wait_poweroff ()
-{
-  local i
-
-  for i in `seq $1`
-  do
-    if [ "$(4gpictl status)" = "off" ]
-    then
-      return 0
-    fi
-    sleep 1
-  done
-
-  return 1
-}
-
 if [ "$AUTO_OFF" != "0" ]
 then
-  if [ "$(4gpictl status)" = "off" ]
-  then
-    exit 0
-  fi
-  eval chat -t $TIMEOUT $ABORT $SCRIPT_FNCMIN < $CONSOLE_PORT > $CONSOLE_PORT
-  eval chat -t $TIMEOUT $ABORT $SCRIPT_PWROFF < $CONSOLE_PORT > $CONSOLE_PORT
-  wait_poweroff $TIMEOUT || 4gpictl poweroff
+	if [ "$(4gpictl status)" = "off" ]
+	then
+		exit 0
+	fi
+	4gpictl poweroff || 4gpictl --hard poweroff
 else
-  if [ "$WAKE_ON_RING" != "0" ]
-  then
-    if [ ! -e $CONSOLE_PORT ]
-    then
-      exit 1
-    fi
-    if [ "$STORE_SMS_ON_RING" != "0" ]
-    then
-      eval chat -t $TIMEOUT $ABORT $SCRIPT_ME < $CONSOLE_PORT > $CONSOLE_PORT
-    else
-      eval chat -t $TIMEOUT $ABORT $SCRIPT_SM < $CONSOLE_PORT > $CONSOLE_PORT
-    fi
-    eval chat -t $TIMEOUT $ABORT $SCRIPT_RI < $CONSOLE_PORT > $CONSOLE_PORT
-  fi
+	if [ "$WAKE_ON_RING" != "0" ]
+	then
+		if [ ! -e $CONSOLE_PORT ]
+		then
+			exit 1
+		fi
+		if [ "$STORE_SMS_ON_RING" != "0" ]
+		then
+			4gpictl store-sms enable
+		else
+			4gpictl store-sms disable
+		fi
+		4gpictl wake-on-ring enable
+	fi
 fi
 
--- modem-config.sh
+++ modem-config.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-TIMEOUT=30
-
-SCRIPT="ABORT BUSY ABORT ERROR '' AT OK-AT-OK AT+CSCLK=0 OK ''"
-
 if [ "$AUTO_OFF" != "0" ]
 then
-  exit 0
+	exit 0
 fi
 
 if [ "$WAKE_ON_RING" != "0" ]
 then
-  eval chat -t $TIMEOUT $SCRIPT < $CONSOLE_PORT > $CONSOLE_PORT
+	4gpictl wake-on-ring disable
 fi
 
